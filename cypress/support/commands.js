f0926b8fcde23225d460bffdcc7e3839eb18dc91
shivanirampally <shivani.rampally@gmail.com>
2026-06-16 20:13:42 +0530

    Cleanup framework and finalize POC branches

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,66 @@
 Cypress.Commands.add("loginMicrosoft", () => {
 
-  cy.session("chorus-token-session", () => {
+  cy.session("chorus-session", () => {
 
-    // ✅ FIX: use full URL to avoid baseUrl prefix
-    cy.visit("about:blank", { failOnStatusCode: false });
-
+ cy.visit("/", {
+      failOnStatusCode: false
+    });
+    
     cy.window().then((win) => {
 
-      const sessionStorage = win.sessionStorage;
+      win.sessionStorage.setItem(
+        "access_token",
+        Cypress.env("access_token")
+      );
 
-      sessionStorage.setItem("access_token", Cypress.env("access_token"));
-      sessionStorage.setItem("id_token", Cypress.env("id_token"));
-      sessionStorage.setItem("session_state", Cypress.env("session_state"));
+      win.sessionStorage.setItem(
+        "id_token",
+        Cypress.env("id_token")
+      );
 
-      if (Cypress.env("id_token_claims_obj")) {
-        sessionStorage.setItem(
-          "id_token_claims_obj",
-          Cypress.env("id_token_claims_obj")
-        );
-      }
+      win.sessionStorage.setItem(
+        "session_state",
+        Cypress.env("session_state")
+      );
 
-      sessionStorage.setItem("expires_at", Date.now() + 3600 * 1000);
-      sessionStorage.setItem("access_token_stored_at", Date.now());
-      sessionStorage.setItem("id_token_stored_at", Date.now());
+      win.sessionStorage.setItem(
+        "nonce",
+        Cypress.env("nonce")
+      );
+
+      win.sessionStorage.setItem(
+        "granted_scopes",
+        Cypress.env("granted_scopes")
+      );
+
+      win.sessionStorage.setItem(
+        "id_token_claims_obj",
+        Cypress.env("id_token_claims_obj")
+      );
+
+      win.sessionStorage.setItem(
+        "access_token_stored_at",
+        Cypress.env("access_token_stored_at")
+      );
+
+      win.sessionStorage.setItem(
+        "id_token_stored_at",
+        Cypress.env("id_token_stored_at")
+      );
+
+      win.sessionStorage.setItem(
+        "expires_at",
+        Cypress.env("expires_at")
+      );
+
+      win.sessionStorage.setItem(
+        "id_token_expires_at",
+        Cypress.env("id_token_expires_at")
+      );
+
     });
-
-    // ✅ Now open actual app
-    cy.visit("/", { failOnStatusCode: false });
 
   });
 
+  cy.visit("/");
 });
--- cypress/support/step_definitions/loginSteps.js
+++ cypress/support/step_definitions/loginSteps.js
@@ -1,18 +1,13 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("I open Chorus application", () => {
   cy.loginMicrosoft();
 });
 
-When("I login using Microsoft credentials", () => {
-  // handled already
-});
-
 Then("I should land on Chorus home page", () => {
-
   cy.url({ timeout: 60000 }).should("include", "chorusqa");
 
-  cy.contains("Learning and Development", { timeout: 60000 })
-    .should("be.visible");
-
+ cy.contains("Shivani Rampally", {
+  timeout: 120000
+}).should("be.visible");
 });
