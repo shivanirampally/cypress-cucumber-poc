12090a240c40de322e638532825e5b76dd82521c
shivanirampally <shivani.rampally@gmail.com>
2026-07-24 17:19:36 +0530

    updated the config with unneccessary installation

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,9 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const {addCucumberPreprocessorPlugin,} = require("@badeball/cypress-cucumber-preprocessor");
-const {createEsbuildPlugin,} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
-async function setupNodeEvents(on, config) {
-  await addCucumberPreprocessorPlugin(on, config);
-  on(
-    "file:preprocessor",
-    createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    })
-  );
-
-  return config;
-}
 
 module.exports = defineConfig({
 
   reporter: "mochawesome",
+
   reporterOptions: {
     reportDir: "cypress/reports",
     overwrite: false,
@@ -26,14 +12,8 @@ module.exports = defineConfig({
   },
 
   e2e: {
-
     baseUrl: "https://reqres.in/api",
-    specPattern: "cypress/e2e/**/*.cy.js",
-
-    async setupNodeEvents(on, config) {
-      return await setupNodeEvents(on, config);
-    }
-
+    specPattern: "cypress/e2e/**/*.cy.js"
   }
 
 });
